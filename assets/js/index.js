2f6ab782a3d8837ca0d861dcccdb873de0458f5a
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-04-24 19:02:26 +0000

    Add responsive design

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+const contactBtns = document.querySelectorAll(".contact-btn");
+
+for (let i = 0; i < contactBtns.length; i++) {
+    contactBtns[i].addEventListener("click", () => {
+        window.location.href = "contact.html";
+    })
+}
+
+const aboutBtns = document.querySelectorAll(".about-btn");
+
+for (let i = 0; i < aboutBtns.length; i++) {
+    aboutBtns[i].addEventListener("click", () => {
+        window.location.href = "about.html";
+    })
+}
