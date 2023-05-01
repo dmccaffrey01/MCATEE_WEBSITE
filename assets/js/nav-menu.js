f00f894218ec34f0cdb5b11177cac69331269449
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-05-01 20:53:24 +0000

    Fix resposnive styling for footer and sections, add nav dropdown, change font sizes, change content for contact page

--- assets/js/nav-menu.js
+++ assets/js/nav-menu.js
@@ -3,13 +3,16 @@
  */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const header = document.getElementsByTagName("header")[0];
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    header.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    header.classList.remove("active");
 }));
