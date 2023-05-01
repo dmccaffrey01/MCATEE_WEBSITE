9d193a8c9c8b5cdb2dedbaaa29df7fcb6e0d5fe8
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-05-01 22:55:50 +0000

    Fix nav menu for mobile and in contact and about pages

--- assets/js/nav-menu.js
+++ assets/js/nav-menu.js
@@ -5,6 +5,11 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const header = document.getElementsByTagName("header")[0];
 
+const navDropdownContainer = document.querySelector(".nav-dropdown-container");
+const navDropdown = document.querySelector(".nav-dropdown");
+const navDropdownLink = document.querySelector(".nav-dropdown-link");
+const navDropdownIcon = document.querySelector(".nav-dropdown-icon");
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
@@ -12,7 +17,25 @@ hamburger.addEventListener("click", () => {
 });
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    if (n.classList.contains("nav-dropdown-link")) {
+        return;
+    }
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
     header.classList.remove("active");
 }));
+
+navDropdownLink.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    navDropdownContainer.classList.toggle("active");
+    
+    if (navDropdownContainer.classList.contains("active")) {
+        const containerHeight = navDropdown.getBoundingClientRect().height;
+        navDropdownContainer.style.height = containerHeight + "px";
+        navDropdownIcon.style.transform = "rotate(-180deg)"
+    } else {
+        navDropdownContainer.style.height = "0";
+        navDropdownIcon.style.transform = "rotate(0)"
+    }
+  });
