44c7ec1ba949d8147c277fbe924818ab848e426a
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-05-02 18:47:32 +0000

    Add buy page, with hero section and morals section

---
+++ assets/js/contact.js
@@ -0,0 +1,88 @@
+const form = document.querySelector(".contact-form")
+
+const nameInput = document.querySelector(".contact-name-input");
+const emailInput = document.querySelector(".contact-email-input");
+const messageInput = document.querySelector(".contact-message-input");
+
+messageInput.addEventListener("focus", () => {
+	if (messageInput.value == "Drop a message to the McAtee Group...") {
+		messageInput.value = "";
+	}
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    sendEmail();
+})
+
+(function(){
+	emailjs.init("zPKR6_gHufVNrHLZT");
+})();
+
+/**
+ * Send email
+ */
+function sendEmail() {
+    
+	const formData = {
+        name: nameInput.value,
+        email: emailInput.value,
+        message: messageInput.value
+    }
+	
+	if (formData.name == "" || formData.email == "") {
+		displayNotification("You must fill in the Name and Email. Please Try again.", "OK", 5000);
+        return false;
+	}
+
+	var templateParams = {
+		from_name: formData.name,
+		to_name: "McAtee Group",
+		from_email: formData.email,
+		message: formData.message,
+		reply_to: formData.email
+	};
+
+	emailjs.send("service_q2d91ik", "template_0j3zj9k", templateParams, "zPKR6_gHufVNrHLZT")
+		.then(function(response) {
+			console.log('SUCCESS!', response.status, response.text);
+			displayNotification("The message was successfully sent to McAtee Group. Thank you!", "OK", 5000);
+			resetForm();
+		}, function(error) {
+			console.log('FAILED...', error);
+			displayNotification("The message failed to send. Try again or send directly to INFO@mcateegroup.com", "OK", 5000);
+		});
+}
+
+/**
+ * Reset the form values
+ */
+function resetForm() {
+	nameInput.value = "";
+	emailInput.value = "";
+	messageInput.value = "Leave a message for Dillon...";
+}
+
+/**
+ * Display notification message
+ */
+function displayNotification(message, btn1, time) {
+	// Define vars
+	let notification = document.querySelector(".notification");
+	let notificationMessage = document.querySelector(".notification-message");
+	let notificationBtn = document.querySelector(".notification-btn");
+
+	notificationMessage.innerText = message;
+
+	notificationBtn.innerText = btn1;
+
+	notification.classList.add("active");
+
+	notificationBtn.addEventListener("click", () => {
+		notification.classList.remove("active");
+	});
+
+    window.setTimeout(() => {
+        notification.classList.remove("active");
+    }, time);
+}
--- assets/js/copy-to-clipboard.js
+++ assets/js/copy-to-clipboard.js
@@ -1,6 +1,5 @@
 const copyLinks = document.querySelectorAll('.copy-link');
-const message = document.querySelector('.notification-message');
-const notification = document.querySelector(".notification");
+
 
 copyLinks.forEach(function (copyLink) {
     copyLink.addEventListener('click', function (event) {
@@ -9,18 +8,36 @@ copyLinks.forEach(function (copyLink) {
 
         navigator.clipboard.writeText(copyText)
             .then(() => {
-                message.textContent = `"${copyText}" - was copied to the clipboard`;
-                notification.classList.add("active");
-                window.setTimeout(() => {
-                    notification.classList.remove("active");
-                }, 3000);
+                let message = `"${copyText}" - was copied to the clipboard`;
+                displayNotification(message, "CLOSE", 3000);
             })
             .catch(() => {
-                message.textContent = 'Failed to copy text to clipboard';
-                notification.classList.add("active");
-                window.setTimeout(() => {
-                    notification.classList.remove("active");
-                }, 3000);
+                let message = 'Failed to copy text to clipboard';
+                displayNotification(message, "CLOSE", 3000);
             });
     });
 });
+
+/**
+ * Display notification message
+ */
+function displayNotification(message, btn1, time) {
+	// Define vars
+	let notification = document.querySelector(".notification");
+	let notificationMessage = document.querySelector(".notification-message");
+	let notificationBtn = document.querySelector(".notification-btn");
+
+	notificationMessage.innerText = message;
+
+	notificationBtn.innerText = btn1;
+
+	notification.classList.add("active");
+
+	notificationBtn.addEventListener("click", () => {
+		notification.classList.remove("active");
+	});
+
+    window.setTimeout(() => {
+        notification.classList.remove("active");
+    }, time);
+}
