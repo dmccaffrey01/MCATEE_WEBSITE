25c68336984384c46d78982193091beab1d730e7
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-05-08 20:41:47 +0100

    Add coaching page

---
+++ assets/js/google-maps.js
@@ -0,0 +1,5 @@
+const googleMapsBtn = document.querySelector(".google-maps-btn");
+
+googleMapsBtn.addEventListener("click", () => {
+    window.open("https://www.google.com/maps/dir//Taylor+Properties+175+Admiral+Cochrane+Dr+suite+111-112+Annapolis,+MD+21401+United+States/@38.9746201,-76.5498768,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89b7f6c5570a20c3:0x6365e0bf6e65a1d5!2m2!1d-76.5498768!2d38.9746201", "_blank");
+})
