c6842583e365a091bfdf00d3cfbfd3d81caba3c6
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-05-15 22:19:18 +0100

    Add 404.js

---
+++ assets/js/404.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.return-home-btn');
+const text = document.querySelector('.error-text');
+
+if (document.location.pathname === '/zoom') {
+    text.textContent = 'Redirecting to zoom...';
+    btn.textContent = 'Go to zoom';
+    window.location.href = 'https://us02web.zoom.us/j/4146842904';
+    btn.setAttribute("href", "https://us02web.zoom.us/j/4146842904");
+}
