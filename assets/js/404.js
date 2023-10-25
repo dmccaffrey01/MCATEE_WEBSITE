234a70a070f81378083996ceb990d6b5ace707cd
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-10-25 10:48:43 +0100

    Add 404

---
+++ assets/js/404.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let currentUrl = new URL(window.location);
+
+    let splitPathname = currentUrl.pathname.split("/");
+
+    let lastPathname = splitPathname[splitPathname.length - 1];
+
+    console.log(lastPathname);
+
+    if (lastPathname == zoom) {
+        currentUrl = "https://us02web.zoom.us/j/4146842904";
+    } else {
+        currentUrl.pathname = splitPathname[0];
+    }
+
+    window.location.replace(currentUrl);
+});
